b593168233ac168ee95bd7ad2a4ab84688db04d4
Xin <vincent7xin@gmail.com>
2019-04-17 19:30:20 +0800

    项目骨架

---
+++ sparrow-admin-api/src/main/java/com/github/ionething/sparrowplatform/SparrowPlatformAdminApplication.java
@@ -0,0 +1,16 @@
+package com.github.ionething.sparrowplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Vincent
+ */
+@SpringBootApplication
+public class SparrowPlatformAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SparrowPlatformAdminApplication.class, args);
+    }
+
+}
---
+++ sparrow-monitor/src/test/java/com/github/ionething/sparrowplatform/monitor/SparrowPlatformMonitorApplicationTest.java
@@ -0,0 +1,16 @@
+package com.github.ionething.sparrowplatform.monitor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {SparrowPlatformMonitorApplication.class})
+public class SparrowPlatformMonitorApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
